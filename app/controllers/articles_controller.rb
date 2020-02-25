f72061f6945fd1ff6deecba7f0d2f6f2624b1f4c
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-25 15:21:10 +0100

    Implemented actions new and create for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,31 @@ class ArticlesController < ApplicationController
   def show
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
   def set_article
     @article = Article.find(params[:id])
